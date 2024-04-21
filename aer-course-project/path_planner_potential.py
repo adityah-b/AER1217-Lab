de14f7378896c18168a2aafa6f1c3122b6f58aa3
Victor-YG <yizhougao8@gmail.com>
2024-04-21 16:36:29 -0400

    Implement the bidirectional exit trajectory.

--- aer-course-project/path_planner_potential.py
+++ aer-course-project/path_planner_potential.py
@@ -58,48 +58,14 @@ class PathPlannerPotential:
         self.gates.append((position[0], position[1], orientation))
 
 
-    # def initialize_trajectory(self, bidirectional=False):
-    #     '''initialize trajectory by connecting the dots'''
-    #
-    #     waypoints = []
-    #     # additional waypoints for smooth take-off (vertical)
-    #     waypoints.append(self.start_state + np.array([0.0, 0.0, TAKE_OFF_HEIGHT]))
-    #     waypoints.append(self.start_state + np.array([0.0, 0.0, TAKE_OFF_HEIGHT * 2]))
-    #     waypoints.append(self.start_state + np.array([0.0, 0.0, TAKE_OFF_HEIGHT * 3]))
-    #     waypoints.append(self.start_state + np.array([0.0, 0.0, TAKE_OFF_HEIGHT * 4]))
-    #     waypoints.append(self.start_state + np.array([0.0, 0.0, 1.0]))
-    #
-    #     prev_point_1 = self.start_state + np.array([0.0, 0.0, 1.0])
-    #     prev_point_2 = self.start_state + np.array([0.0, 0.0, 1.0])
-    #
-    #     for (x, y, theta) in self.gates:
-    #         center = np.array([x, y, GATE_CENTER_HEIGHT])
-    #         offset = GATE_CENTER_OFFSET
-    #         offset = offset * np.array([np.sin(theta), np.cos(theta), 0.0])
-    #         front  = center + offset
-    #         back   = center - offset
-    #
-    #         if bidirectional:
-    #             pass
-    #         else:
-    #             if np.linalg.norm(prev_point_1 - front) < np.linalg.norm(prev_point_1 - back):
-    #                 waypoints.append(front)
-    #                 waypoints.append(back)
-    #                 prev_point_1 = back
-    #             else:
-    #                 waypoints.append(back)
-    #                 waypoints.append(front)
-    #                 prev_point_1 = front
-    #
-    #     # vertical landing
-    #     waypoints.append(self.end_state + np.array([0.0, 0.0, 1.0]))
-    #     waypoints.append(self.end_state + np.array([0.0, 0.0, LANDING_HEIGHT]))
-    #
-    #     self.initial_waypoints = waypoints
-    #     return waypoints
-
-
     def plan_trajectory(self):
+        if USE_BIDIRECTIONAL_EXIT:
+            return self.plan_trajectory_bidirectional()
+        else:
+            return self.plan_trajectory_unidirectional()
+
+
+    def plan_trajectory_unidirectional(self):
         waypoints = []
 
         # waypoints for smooth take-off (vertical)
@@ -163,17 +129,165 @@ class PathPlannerPotential:
         return self.final_waypoints, self.speeds
 
 
+    def plan_trajectory_bidirectional(self):
+        waypoints = []
+
+        # waypoints for smooth take-off (vertical)
+        self.final_waypoints.append(self.start_state + np.array([0.0, 0.0, TAKE_OFF_HEIGHT]))
+        self.speeds.append(0.0)
+        self.final_waypoints.append(self.start_state + np.array([0.0, 0.0, TAKE_OFF_HEIGHT * 2]))
+        self.speeds.append(0.0)
+        self.final_waypoints.append(self.start_state + np.array([0.0, 0.0, TAKE_OFF_HEIGHT * 3]))
+        self.speeds.append(0.0)
+        self.final_waypoints.append(self.start_state + np.array([0.0, 0.0, TAKE_OFF_HEIGHT * 4]))
+        self.speeds.append(0.0)
+        # self.final_waypoints.append(self.start_state + np.array([0.0, 0.0, 1.0]))
+        # self.speeds.append(0.0)
+
+        prev_front_point = self.start_state + np.array([0.0, 0.0, 1.0])
+        prev_back_point  = self.start_state + np.array([0.0, 0.0, 1.0])
+        prev_entry_point = prev_front_point
+        prev_exit_point  = prev_back_point
+        prev_entry_point_key = "front"
+        prev_exit_point_key = "back"
+        curr_front_point = None
+        curr_back_point  = None
+        curr_entry_point = None
+        curr_exit_point  = None
+        curr_entry_point_key = None
+        curr_exit_point_key = None
+
+        for (x, y, theta) in self.gates:
+            center = np.array([x, y, GATE_CENTER_HEIGHT])
+            offset = GATE_CENTER_OFFSET
+            offset = offset * np.array([np.sin(theta), np.cos(theta), 0.0])
+            curr_front_point  = center + offset
+            curr_back_point   = center - offset
+
+            # determine entry direction
+            shortest_distance = 1000
+            shortest_connection = None
+            dist_front_to_front = np.linalg.norm(prev_front_point - curr_front_point)
+            dist_front_to_back  = np.linalg.norm(prev_front_point - curr_back_point)
+            dist_back_to_front  = np.linalg.norm(prev_back_point  - curr_front_point)
+            dist_back_to_back   = np.linalg.norm(prev_back_point  - curr_back_point)
+
+            if dist_front_to_front < shortest_distance:
+                shortest_distance = dist_front_to_front
+                shortest_connection = ("front", "front")
+                prev_exit_point_key = "front"
+                curr_entry_point_key = "front"
+            if dist_front_to_back < shortest_distance:
+                shortest_distance = dist_front_to_back
+                shortest_connection = ("front", "back")
+                prev_exit_point_key = "front"
+                curr_entry_point_key = "back"
+            if dist_back_to_front < shortest_distance:
+                shortest_distance = dist_back_to_front
+                shortest_connection = ("back", "front")
+                prev_exit_point_key = "back"
+                curr_entry_point_key = "front"
+            if dist_back_to_back < shortest_distance:
+                shortest_distance = dist_back_to_back
+                shortest_connection = ("back", "back")
+                prev_exit_point_key = "back"
+                curr_entry_point_key = "back"
+
+            # add the opposite point if entry and exit is the same
+            if prev_exit_point_key == prev_entry_point_key:
+                self.speeds.append(WAYPOINT_TRACKING_SPEED_MIN)
+                if prev_exit_point_key == "front":
+                    self.final_waypoints.append(prev_back_point)
+                if prev_exit_point_key == "back":
+                    self.final_waypoints.append(prev_front_point)
+
+            if prev_exit_point_key == "front":
+                prev_entry_point = prev_back_point
+                prev_exit_point  = prev_front_point
+            else:
+                prev_entry_point = prev_front_point
+                prev_exit_point  = prev_back_point
+
+            if curr_entry_point_key == "front":
+                curr_entry_point = curr_front_point
+                curr_exit_point  = curr_back_point
+            else:
+                curr_entry_point = curr_back_point
+                curr_exit_point  = curr_front_point
+
+            # prev_entry_point = prev_front_point if prev_entry_point_key == "front" else prev_back_point
+            # prev_exit_point  = prev_front_point if prev_exit_point_key  == "front" else prev_back_point
+
+            # curr_entry_point = curr_front_point if curr_entry_point_key == "front" else curr_back_point
+            # curr_exit_point  = curr_front_point if curr_exit_point_key  == "front" else curr_back_point
+
+            # optimize trajectory and velocity
+            points = self.path_planning(prev_entry_point, prev_exit_point, curr_entry_point, curr_exit_point)
+            speeds = self.compute_speeds(prev_entry_point, prev_exit_point, curr_entry_point, curr_exit_point, points, self.speeds[-1])
+            self.final_waypoints.append(prev_exit_point)
+            self.final_waypoints.extend(points)
+            self.final_waypoints.append(curr_entry_point)
+            self.speeds.extend(speeds)
+
+            prev_front_point = curr_front_point
+            prev_back_point = curr_back_point
+            prev_entry_point = curr_entry_point
+            prev_exit_point  = curr_exit_point
+            prev_entry_point_key = curr_entry_point_key
+            prev_exit_point_key = curr_exit_point_key
+
+        # find optimal trajectory for reaching end state
+        dist_to_front = np.linalg.norm(self.end_state - prev_front_point)
+        dist_to_back  = np.linalg.norm(self.end_state - prev_back_point)
+
+        if dist_to_front < dist_to_back:
+            prev_exit_point_key = "front"
+            prev_entry_point = prev_back_point
+            prev_exit_point = prev_front_point
+        else:
+            prev_exit_point_key = "back"
+            prev_entry_point = prev_front_point
+            prev_exit_point = prev_back_point
+
+        if prev_exit_point_key == prev_entry_point_key:
+            self.speeds.append(SMOOTH_TRACKING_SPEED_MIN)
+            if prev_exit_point_key == "front":
+                self.final_waypoints.append(prev_back_point)
+            else:
+                self.final_waypoints.append(prev_front_point)
+
+        points = self.path_planning(prev_entry_point, prev_exit_point, self.end_state, self.end_state)
+        speeds = self.compute_speeds(prev_entry_point, prev_exit_point, self.end_state, self.end_state, points, self.speeds[-1])
+        self.final_waypoints.append(prev_exit_point)
+        self.final_waypoints.extend(points)
+        self.speeds.extend(speeds[0: -1])
+
+        # vertical landing
+        self.final_waypoints.append(self.end_state + np.array([0.0, 0.0, 1.0]))
+        self.speeds.append(0.0)
+        self.final_waypoints.append(self.end_state + np.array([0.0, 0.0, LANDING_HEIGHT]))
+        self.speeds.append(0.0)
+
+        # plot final trajectory
+        if POT_PLOT_FINAL_TRAJECTORY: self.plot_trajectory()
+        return self.final_waypoints, self.speeds
+
+
     def path_planning(self, prev_entry_point, prev_exit_point, curr_entry_point, curr_exit_point):
         '''find path from start position to goal position'''
 
         # initialize waypoints
-        NUM_SUB_SAMPLE = int(np.linalg.norm(curr_entry_point - prev_exit_point) / SUB_SAMPLE_DISTANCE)
-        print(f"Subsample {NUM_SUB_SAMPLE} points from {prev_exit_point} to {curr_entry_point}")
-        scales = np.linspace(0.0, 1.0, num=(NUM_SUB_SAMPLE + 2))
+        if SUB_SAMPLE_DISTANCE == None:
+            num_sub_sample = NUM_SUB_SAMPLE
+        else:
+            num_sub_sample = int(np.linalg.norm(curr_entry_point - prev_exit_point) / SUB_SAMPLE_DISTANCE)
+            print(f"Subsample {num_sub_sample} points from {prev_exit_point} to {curr_entry_point}")
+
+        scales = np.linspace(0.0, 1.0, num=(num_sub_sample + 2))
         scales = scales[1: -1].reshape([-1, 1])
 
         diff = curr_entry_point - prev_exit_point
-        points = np.tile(diff[0:2], (NUM_SUB_SAMPLE, 1)) * scales + prev_exit_point[0:2]
+        points = np.tile(diff[0:2], (num_sub_sample, 1)) * scales + prev_exit_point[0:2]
 
         # optimize waypoints
         def waypoint_loss(points):
@@ -186,14 +300,14 @@ class PathPlannerPotential:
         def length_loss(points):
             loss = 0.0
             loss += (points[0] - prev_exit_point[0])**2 + (points[1] - prev_exit_point[1])**2
-            for i in range(NUM_SUB_SAMPLE - 1):
+            for i in range(num_sub_sample - 1):
                 loss += (points[2 * (i + 1)] - points[2 * i])**2 + (points[2 * (i + 1) + 1] - points[2 * i + 1])**2
             loss += (curr_entry_point[0] - points[-2])**2 + (curr_entry_point[1] - points[-1])**2
             return loss
 
         def collision_loss(points):
             loss = 0.0
-            for i in range(NUM_SUB_SAMPLE):
+            for i in range(num_sub_sample):
                 loss += self.env.compute_cost(np.array([points[2 * i], points[2 * i + 1], 1.0]))
             return loss
 
@@ -207,7 +321,7 @@ class PathPlannerPotential:
             v2 = np.array([points[2] - points[0], points[3] - points[1]])
             loss -= np.dot(v1, v2) / (np.linalg.norm(v1) + 1e-6) / (np.linalg.norm(v2) + 1e-6)
 
-            for i in range(1, NUM_SUB_SAMPLE - 1):
+            for i in range(1, num_sub_sample - 1):
                 v1 = np.array([points[2 * i] - points[2 * (i - 1)], points[2 * i + 1] - points[2 * (i - 1) + 1]])
                 v2 = np.array([points[2 * (i + 1)] - points[2 * i], points[2 * (i + 1) + 1] - points[2 * i + 1]])
                 loss -= np.dot(v1, v2) / (np.linalg.norm(v1) + 1e-6) / (np.linalg.norm(v2) + 1e-6)
@@ -225,7 +339,7 @@ class PathPlannerPotential:
         res = minimize(waypoint_loss, points, method='BFGS', options={'xatol': 1e-6, 'disp': True})
 
         print(res.x)
-        points = res.x.reshape([NUM_SUB_SAMPLE, 2])
+        points = res.x.reshape([num_sub_sample, 2])
 
         # visualize
         if POT_PLOT_SUB_TRAJECTORY:
@@ -241,7 +355,7 @@ class PathPlannerPotential:
             plt.show()
 
         waypoints = []
-        for i in range(NUM_SUB_SAMPLE):
+        for i in range(num_sub_sample):
             waypoints.append(np.array([points[i, 0], points[i, 1], 1.0]))
 
         return waypoints
